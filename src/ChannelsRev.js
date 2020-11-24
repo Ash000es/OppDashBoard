12aa47d4ea3c0f7d07b9c82ae1d625b9d5905a1a
ash000es <ash007.uk@icloud.com>
2020-11-24 19:34:57 +0100

    final pie chart changes

--- src/ChannelsRev.js
+++ src/ChannelsRev.js
@@ -1,77 +1,122 @@
 import React, { useCallback, useEffect, useState } from 'react'
 import { tripleValuesCompare } from './data/bookings-revenue-data'
 import { makeStyles } from '@material-ui/core/styles'
-import { PieChart,Pie, Tooltip  } from 'recharts'
+import { PieChart, Pie, Tooltip,Sector, Cell } from 'recharts'
 const useStyles = makeStyles((theme) => ({
-    root: {
-      'max-width': '100%',
-      margin: '1rem auto',
-      display:'flex',
-      justifyContent:'flex-start',
-      border:'1px solid black'
-    }, 
-    info:{
-        width:'35%',
-        // border:'1px solid green',
-        paddingLeft:'5rem'
-
-    },
-    chart:{
-        width:'65%',
-        // border:'1px solid red',
-        paddingRight:'2rem'
-        
-        
-    }
-  }))
+  root: {
+    'width': 800,
+    'height': 550,
+    margin: '1rem auto',
+    display: 'flex',
+    justifyContent: 'flex-start',
+    border: '1px solid black',
+  },
+  info: {
+    width: '35%',
+    // border: '1px solid green',
+    paddingLeft: '2rem',
+  },
+  chart: {
+    width: '65%',
+    // margin: 10,
+    // border: '1px solid red',
+    // paddingRight: '2rem',
+  },
+}))
 const reducedValue = (arr) => {
-    return arr.reduce((prev, cur) => {
-      return { revenue: parseInt(prev.revenue+ cur.revenue) }
-    })
-  }
-  
+  return arr.reduce((prev, cur) => {
+    return { revenue: parseInt(prev.revenue + cur.revenue) }
+  })
+}
+
 export const ChannelsRev = () => {
-    const classes = useStyles()
+  const classes = useStyles()
   const [data, setData] = useState([])
   const [totalrev, setTotalRev] = useState({})
   const [channels, setChannels] = useState([
-    {name:'Airbnb', value:452500},
-    {name:'Booking', value:206250},
-    {name:'Expedia', value:206250},
-    {name:'Vrbo', value:40000}
+    { name: 'Airbnb', value: 452500 },
+    { name: 'Booking', value: 206250 },
+    { name: 'Expedia', value: 206250 },
+    { name: 'Vrbo', value: 40000 }
   ])
 
   useEffect(() => {
-      function getData(arr){
-        setData(arr)
-        const res= reducedValue(arr)
-        setTotalRev(res)
-        
-      }
-      getData(tripleValuesCompare)
-    
+    function getData(arr) {
+      setData(arr)
+      const res = reducedValue(arr)
+      setTotalRev(res)
+    }
+    getData(tripleValuesCompare)
+  }, [])
 
 
 
-}, [])
+  const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
+
+const RADIAN = Math.PI / 180;
+const renderCustomizedLabel = ({
+  cx, cy, midAngle, innerRadius, outerRadius, percent, index,
+}) => {
+   const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+  const x = cx + radius * Math.cos(-midAngle * RADIAN);
+  const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+  return (
+    <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
+      {`${(percent * 100).toFixed(0)}%`}
+    </text>
+  );
+};
   return (
     <div className={classes.root}>
-    <div className={classes.info}>
-      <h4>Aggregated value of revenue is ${totalrev.revenue} </h4>
-      <h4> Break down per channel is {channels.map(chan=>{
-          return(
-          <ul chan={chan} key={chan.name}>
-           <li style={{listStyle:'none'}}>{chan.name}:{chan.value}$</li>   
-          </ul>
-       ) })}</h4>
-       </div>
-       <div className={classes.chart}>
-      <PieChart width={800} height={450} paddingAngle={40}>
-      <Tooltip/>
-  <Pie data={data} dataKey="revenue" nameKey="month" cx="50%" cy="50%" outerRadius={100} fill="#8884d8" />
-  <Pie data={channels} dataKey="value" nameKey="name" cx="50%" cy="50%" innerRadius={120} outerRadius={160} fill="#82ca9d" label />
-</PieChart>
-   </div>
+      <div className={classes.info}>
+        <h4>Aggregated value of revenue is ${totalrev.revenue} </h4>
+        <h4>
+          {' '}
+          Break down per channel is{' '}
+          {channels.map((chan) => {
+            return (
+              <ul chan={chan} key={chan.name}>
+                <li style={{ listStyle: 'none' }}>
+                  {chan.name}:{chan.value}$
+                </li>
+              </ul>
+            )
+          })}
+        </h4>
+      </div>
+      <div className={classes.chart}>
+        <PieChart width={500} height={400} paddingAngle={40}  >
+      
+          <Pie
+            data={data}
+            dataKey="revenue"
+            nameKey="month"
+            cx="50%"
+            cy="50%"
+            outerRadius={75}
+            fill="#8884d8"
+            
+          />
+          <Pie
+          data={channels}
+          cx="50%"
+            cy="50%"
+          labelLine={false}
+          label={renderCustomizedLabel}
+          innerRadius={100}
+            outerRadius={150}
+          fill="#8884d8"
+          nameKey="name"
+          dataKey="value"
+        >
+          {
+            data.map((entry, index) => <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />)
+          }
+        </Pie>
+         <Tooltip />
+        </PieChart>
+      </div>
     </div>
   )
 }
